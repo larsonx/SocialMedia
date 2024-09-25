163ed824778a79b933b531e67c3e6efc5dc6f09c
larson cop <97100495@st.deltion.nl>
2024-09-25 09:39:23 +0200

    Navbar logo added + styling

--- resources/views/components/navbarlogin.blade.php
+++ resources/views/components/navbarlogin.blade.php
@@ -1,28 +1,19 @@
-@if (Route::has('login'))
-    <nav class="bg-yellow-g flex flex-1 justify-end h-14">
-        @auth
-            <a
-                href="{{ url('/dashboard') }}"
-                class="rounded-md px-3 py-2"
-            >
-                Dashboard
-            </a>
-        @else
-            <a
-                href="{{ route('login') }}"
-                class="rounded-md px-2 py-4"
-            >
-                Log in
-            </a>
-
-            @if (Route::has('register'))
-                <a
-                    href="{{ route('register') }}"
-                    class="rounded-md px-4 py-4"
-                >
-                    Register
-                </a>
-            @endif
-        @endauth
-    </nav>
-@endif
+    @if (Route::has('login'))
+    <div>
+        <nav class="bg-yellow-g flex justify-between items-center h-14">
+            <div class="flex items-center">
+                <img class="w-24 h-24" src="/logo.png" alt="Logo">
+            </div>
+            <div class="flex items-center space-x-4">
+                @auth
+                    <a href="{{ url('/dashboard') }}" class="rounded-md px-3 py-2">Dashboard</a>
+                @else
+                    <a href="{{ route('login') }}" class="rounded-md px-2 py-3">Log in</a>
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}" class="rounded-md px-3 py-3">Register</a>
+                    @endif
+                @endauth
+            </div>
+        </nav>
+    </div>
+    @endif
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,13 +9,9 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
     </head>
+    <body>
     <x-navbarlogin/>
     <main>
-<div>
-
-    <div>
-
-
     </main>
     </body>
 </html>
