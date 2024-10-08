6507de77dcf063c4eff1b9e3e4d88e2bfc18fa23
larson cop <97100495@st.deltion.nl>
2024-10-08 11:24:43 +0200

    Display data controller

---
+++ app/Http/Controllers/DisplayController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+class DisplayController extends Controller
+{
+    public function show(Request $request): View
+    {
+        return view('profiel', [
+            'user' => $request->user(),
+        ]);
+    }
+}
