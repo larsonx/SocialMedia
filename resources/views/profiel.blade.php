fa3df9c8f71943004e914eea16f640ed5c66ff8f
larson cop <97100495@st.deltion.nl>
2024-10-09 09:52:03 +0200

    Width changes
    Width changes for more responsiveness on profile page

--- resources/views/profiel.blade.php
+++ resources/views/profiel.blade.php
@@ -10,7 +10,7 @@
                 <li><a href="/messages" class="hover:text-blue-500">Messages</a></li>
             </ul>
         </div>
-        <div class="flex h-96 w-4/5 bg-white p-6 shadow-lg rounded-md mx-4">
+        <div class="flex h-96 w-full md:w-4/5 bg-white p-6 shadow-lg rounded-md mx-4">
             <div class="flex flex-col items-left">
                 <div class="w-32 h-32 rounded-full overflow-hidden border-2 border-gray-300">
                     <img id="images" src="{{ asset('images/'.$user->image) }}" alt="Profile Image" class="w-full h-full object-cover"/>
@@ -28,7 +28,7 @@
         </div>
     </div>
     <div class="flex items-center mt-4 justify-center">
-    <div class="flex items-center justify-center  h-96 w-full bg-white p-6 shadow-lg rounded-md w-1/5 mx-4">
+    <div class="flex items-center justify-center h-96 w-full bg-white p-6 shadow-lg rounded-md mx-4">
     </div>
     </div>
     <x-footer/>
