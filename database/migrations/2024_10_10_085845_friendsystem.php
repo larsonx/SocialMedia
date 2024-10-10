8578e8b4939fd9be192667e49f43c6273cb8af02
larson cop <97100495@st.deltion.nl>
2024-10-10 11:01:30 +0200

    friendsystem migration

--- database/migrations/2024_10_10_085845_friendsystem.php
+++ database/migrations/2024_10_10_085845_friendsystem.php
@@ -1,4 +1,5 @@
 <?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,12 +13,10 @@ return new class extends Migration
     {
         Schema::create('friends', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->foreignId('friend_id')->constrained('users')->onDelete('cascade');
-            $table->enum('status', ['pending', 'accepted', 'declined'])->default('pending');
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('friend_id')->constrained('users');
+            $table->integer('accepted')->default(0);
             $table->timestamps();
-            
-            $table->unique(['user_id', 'friend_id']); // Prevent duplicate entries
         });
     }
 
