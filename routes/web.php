6b4913331564a0ba8f4dc1d642ed55f28c199953
larson cop <97100495@st.deltion.nl>
2024-10-23 09:28:37 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -33,18 +33,20 @@ Route::get('/profiel', function () {
     return view('profiel');
 })->middleware(['auth', 'verified'])->name(name: 'dashboard');
 
+//zorgt voor de display van profiel data
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/profiel', [ProfileDataController::class, 'show'])->name('profiel');
 return view ('profiel');
 });
 
+// zorgt voor het editen updaten en vernietigen van het profiel
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-// Voeg namen toe aan de routes
+// Zorgt ervoor dat de posts kunnen worden aangemaakt en verwijderd
 Route::post('/posts', [PostController::class, 'store'])->middleware('auth')->name('posts.store');
 Route::delete('/posts/{post}', [PostController::class, 'destroy'])->middleware('auth')->name('posts.destroy');
 Route::get('/home', [PostController::class, 'index'])->name('home');
