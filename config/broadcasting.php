202c29d5900f47a8e5f4dc62603bc44f8d8b99ae
larson cop <97100495@st.deltion.nl>
2024-11-11 08:51:52 +0100

    .env file and broadcasting setup is GOOD

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -48,21 +48,15 @@ return [
 
         'pusher' => [
             'driver' => 'pusher',
-            'key' => env('e6ad8e79a62255b52122'),
-            'secret' => env('211aea6b8510306e316f'),
-            'app_id' => env('1885374'),
+            'key' => env('PUSHER_APP_KEY'),
+            'secret' => env('PUSHER_APP_SECRET'),
+            'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'cluster' => env('eu'),
-                'host' => env('PUSHER_HOST') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com',
-                'port' => env('PUSHER_PORT', 443),
-                'scheme' => env('PUSHER_SCHEME', 'https'),
-                'encrypted' => true,
-                'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
-            ],
-            'client_options' => [
-                // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
+            'cluster' => env('PUSHER_APP_CLUSTER'),
+            'useTLS' => true,
             ],
         ],
+
 
         'ably' => [
             'driver' => 'ably',
