728881760e12a89f65e60798a6c1207ec6415214
larson cop <97100495@st.deltion.nl>
2024-11-11 09:25:21 +0100

    pusher change

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,11 +1,12 @@
 import Echo from 'laravel-echo';
- 
 import Pusher from 'pusher-js';
-window.Pusher = Pusher;
+ 
+const options = {
+    broadcaster: 'pusher',
+    key: 'your-pusher-channels-key'
+}
  
 window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: import.meta.env.VITE_PUSHER_APP_KEY,
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-    forceTLS: true
+    ...options,
+    client: new Pusher(options.key, options)
 });
