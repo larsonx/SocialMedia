77cea1aee8e0bbe91f66c181646d28fea202431f
larson cop <97100495@st.deltion.nl>
2024-11-14 15:50:32 +0100

    lacking

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -3,11 +3,3 @@ import './echo';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-
-window.Echo.private(`chat.${friendId}`)
-    .listen('MessageSent', (e) => {
-        // Display the incoming message (e.message) on the chat UI
-        console.log('Message received:', e.message);
-        // Update your UI with the new message
-    });
