667a7609f6e57d33af04caf877e8bea90ead9b45
larson cop <97100495@st.deltion.nl>
2024-11-15 16:18:36 +0100

    Update Message.php

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -17,7 +17,7 @@ class Message extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'user_id');
     }
 
     public function friend()
